172fe582b1e0dfb550ffdc6d5bf1cbef89801ef3
elad619 <elad619@gmail.com>
2023-01-07 12:59:26 +0200

    fixes

--- streamlit_utils/streamlit_search.py
+++ streamlit_utils/streamlit_search.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
-from PIL import Image
 from bokeh.models.widgets import Div
 
 from nli_searcher import searcher
